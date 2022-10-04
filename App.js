bc9613f672cb9c3487325a616259412cdd683775
Nikhil Rachawar <nikhil123rachawar@gmail.com>
2022-10-05 00:01:39 +0530

    GOAL RENDERING DONE

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [goalInput, setGoalInput] = useState("");
+  const [goalList, setGoalList] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setGoalInput(enteredText);
+  };
+  const addGoalHandler = () => {
+    setGoalList(prevState=> [...prevState, goalInput]);
+    setGoalInput('');
+  };
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Place your goal' />
-        <Button title='Add Goal'/>
+        <TextInput
+          style={styles.textInput}
+          value={goalInput}
+          onChangeText={goalInputHandler}
+          placeholder="Place your goal"
+        />
+        <Button title="Add Goal"  onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
-        <Text>Goal Names</Text>
+        {goalList.map((goal, id)=><Text key={id}>{goal}</Text>)}
       </View>
     </View>
   );
@@ -20,24 +36,23 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
-
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems:  'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 24,
     borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
+    borderBottomColor: "#cccccc",
     flex: 1,
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    padding: 8
+    borderColor: "#cccccc",
+    width: "70%",
+    padding: 8,
   },
   goalsContainer: {
     flex: 5,
-  }
+  },
 });
